38fd3f8487e1a8b67f4ca9d3be5fe70c151ddb7a
Thanh Sang <dothanhsang95@gmail.com>
2020-07-21 15:37:09 +0700

    sang.dothanh: before #9: Migration database

---
+++ sShopSolution.Data/EF/SShopDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace sShopSolution.Data.EF
+{
+    public class SShopDbContextFactory : IDesignTimeDbContextFactory<SShopDbContext>
+    {
+        public SShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("sShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<SShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new SShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
